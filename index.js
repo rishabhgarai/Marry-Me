6b3809d7df92d4050a25a6958b70db53743351af
rishabhgarai <141753696+rishabhgarai@users.noreply.github.com>
2024-06-01 22:11:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var b = document.querySelector(".noButton");
 
 b.addEventListener("mouseover", function() {
-    const i = Math.floor(Math.random() * 100) + 1; 
-    const j = Math.floor(Math.random() * 100) + 1; 
+    const i = Math.floor(Math.random() * 400) + 1; 
+    const j = Math.floor(Math.random() * 400) + 1; 
 
     b.style.left = i + "px"; 
     b.style.top = j + "px"; 
